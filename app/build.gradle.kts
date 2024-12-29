4b48418067d7c1bb8abcfc650de9036040305650
Viktor Streltsov <streltsov95va@gmail.com>
2024-12-29 16:46:40 +0300

    build(dependencies): add logger SLF4J

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("io.javalin:javalin:6.4.0")
+    implementation("org.slf4j:slf4j-simple:2.0.16")
 }
 
 tasks.test {
