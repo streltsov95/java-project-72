fc78ae87ef80fae815e52531cda72d54a72dc3e1
Viktor Streltsov <streltsov95va@gmail.com>
2025-01-05 16:31:00 +0300

    feat: add Url model

---
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -0,0 +1,37 @@
+package hexlet.code.model;
+
+import java.sql.Timestamp;
+
+public class Url {
+    private int id;
+    private String name;
+    private Timestamp createdAt;
+
+    public Url(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+}
