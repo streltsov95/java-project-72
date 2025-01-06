5725891dfa3b5d27dc45f05e3a544349240750ed
Viktor Streltsov <streltsov95va@gmail.com>
2025-01-06 17:27:09 +0300

    feat: init DB when app configured

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -7,20 +7,29 @@ import io.javalin.Javalin;
 
 import hexlet.code.util.Util;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, SQLException {
         Javalin app = getApp();
         app.start(Util.getPort());
     }
 
-    public static Javalin getApp() {
+    public static Javalin getApp() throws IOException, SQLException {
 
         HikariConfig hikariConfig = new HikariConfig();
         hikariConfig.setJdbcUrl(Util.getDatabaseUrl());
 
         HikariDataSource dataSource = new HikariDataSource(hikariConfig);
         BaseRepository.dataSource = dataSource;
+
+        var initDatabaseSql = Util.readResourceFile("schema.sql");
+        try (var connection = dataSource.getConnection();
+                var statement = connection.createStatement()) {
+            statement.execute(initDatabaseSql);
+        }
 
         Javalin app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
