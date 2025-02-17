e673dbf6fb01caf3484d655d8d0e8eb0a394ccfb
Viktor Streltsov <streltsov95va@gmail.com>
2025-02-17 15:27:25 +0300

    add: controller for root page

---
+++ app/src/main/java/hexlet/code/controller/RootController.java
@@ -0,0 +1,15 @@
+package hexlet.code.controller;
+
+import hexlet.code.dto.BasePage;
+import io.javalin.http.Context;
+
+import static io.javalin.rendering.template.TemplateUtil.model;
+
+public class RootController {
+    public static void index(Context ctx) {
+        var page = new BasePage();
+        page.setFlash(ctx.consumeSessionAttribute("flash"));
+        page.setFlashType(ctx.consumeSessionAttribute("flash-type"));
+        ctx.render("index.jte", model("page", page));
+    }
+}
