d483987673ba11df60d1cef5e683050d04a468fd
Viktor Streltsov <streltsov95va@gmail.com>
2025-02-17 15:21:23 +0300

    add: DTO for Url

---
+++ app/src/main/java/hexlet/code/dto/urls/UrlPage.java
@@ -0,0 +1,16 @@
+package hexlet.code.dto.urls;
+
+import hexlet.code.dto.BasePage;
+import hexlet.code.model.Url;
+
+public class UrlPage extends BasePage {
+    private Url url;
+
+    public UrlPage (Url url) {
+        this.url = url;
+    }
+
+    public Url getUrl() {
+        return url;
+    }
+}
