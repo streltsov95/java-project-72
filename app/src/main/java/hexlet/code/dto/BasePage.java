dbf8bce3f6bd41976ae9a654e9acd95de3fd6ecc
Viktor Streltsov <streltsov95va@gmail.com>
2025-02-17 15:08:54 +0300

    add: base DTO

---
+++ app/src/main/java/hexlet/code/dto/BasePage.java
@@ -0,0 +1,22 @@
+package hexlet.code.dto;
+
+public class BasePage {
+    private String flash;
+    private String flashType;
+
+    public String getFlash() {
+        return flash;
+    }
+
+    public void setFlash(String flash) {
+        this.flash = flash;
+    }
+
+    public String getFlashType() {
+        return flashType;
+    }
+
+    public void setFlashType(String flashType) {
+        this.flashType = flashType;
+    }
+}
