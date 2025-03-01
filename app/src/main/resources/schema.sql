bddc412371cdb1e00a6194674c92ccbb50a92824
Viktor Streltsov <streltsov95va@gmail.com>
2025-03-01 14:48:01 +0300

    feat: add url_checks table to DB

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -1,7 +1,18 @@
 DROP TABLE IF EXISTS urls;
+DROP TABLE IF EXISTS url_checks;
 
 CREATE TABLE urls (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     createdAt TIMESTAMP
+);
+
+CREATE TABLE url_checks (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    url_id INT REFERENCES urls(id) NOT NULL,
+    status_code INT,
+    h1 VARCHAR(255),
+    title VARCHAR(255),
+    description TEXT,
+    created_at TIMESTAMP
 );
