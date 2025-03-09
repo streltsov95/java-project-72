a3c10bb5fd0456b8a7bc503165f3a711f2c04bce
Viktor Streltsov <streltsov95va@gmail.com>
2025-03-09 16:13:32 +0300

    style: '.*' form of test import change to all used methods

--- app/src/test/java/hexlet/code/util/UtilTest.java
+++ app/src/test/java/hexlet/code/util/UtilTest.java
@@ -5,7 +5,8 @@ import org.junit.jupiter.api.Test;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class UtilTest {
 
