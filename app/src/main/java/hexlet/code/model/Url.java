72f88cfe16cb3f653faf21963a232ba5787c2dd4
Viktor Streltsov <streltsov95va@gmail.com>
2025-03-24 17:31:34 +0300

    style: remove commented code

--- app/src/main/java/hexlet/code/model/Url.java
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -4,12 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.sql.Timestamp;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
 
 @Getter
 @Setter
@@ -22,19 +18,4 @@ public final class Url {
     public Url(String name) {
         this.name = name;
     }
-
-//    public Optional<UrlCheck> getLatestUrlCheck() {
-//        if (!getUrlChecks().isEmpty()) {
-//            return getUrlChecks().stream()
-//                    .sorted(Comparator.comparing(UrlCheck::getCreatedAt))
-//                    .reduce((e1, e2) -> e2);
-//        }
-//        return Optional.empty();
-//    }
-
-//    public static String getParsedCreatedAt(Timestamp createdAt) {
-//        LocalDateTime parsedCreatedAt = createdAt.toLocalDateTime();
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
-//        return parsedCreatedAt.format(formatter);
-//    }
 }
